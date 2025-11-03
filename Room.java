6f6da7a0340834806009fc0b9c8f92c60d07fe62
Sean Corrigan <seancorr2007@gmail.com>
2025-11-03 13:39:01 +0000

    Initial commit of Chris's code

---
+++ Room.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Room {
+
+  private String description;
+  private Map<String, Room> exits; // Map direction to neighboring Room
+  private final List<Item> items = new ArrayList<>();
+
+  public Room(String description) {
+    this.description = description;
+    exits = new HashMap<>();
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setExit(String direction, Room neighbor) {
+    exits.put(direction, neighbor);
+  }
+
+  public Room getExit(String direction) {
+    return exits.get(direction);
+  }
+
+  public String getExitString() {
+    StringBuilder sb = new StringBuilder();
+    for (String direction : exits.keySet()) {
+      sb.append(direction).append(" ");
+    }
+    return sb.toString().trim();
+  }
+
+  public String getLongDescription() {
+    return "You are " + description + ".\nExits: " + getExitString();
+  }
+
+  public void addItem(Item item) {
+    if (item == null) throw new IllegalArgumentException("item cannot be null");
+    items.add(item);
+  }
+
+  public List<Item> getItemsInRoom() {
+    return items;
+  }
+}
